35c9ed420f5809c9b30b8ec2b8771143fcf3e946
M.ELSQA®™ <M.ELSQA@AELSHAF3Y>
2026-01-03 17:30:10 +0200

    Add project files.

---
+++ assignment1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Estimate for carpet cleaning service-_-");
+            Console.WriteLine("Number of small carpets: ");
+            int SCarpets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number of large carpets: ");
+            int LCarpets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Price per small carpet: $25");
+            Console.WriteLine("Price per large carpet: $35");
+            int cost = (SCarpets * 25) + (LCarpets * 35);
+            Console.WriteLine($"Cost: ${cost}");
+            double tax = cost * 0.06;
+            Console.WriteLine($"Tax: ${tax}");
+            double total = cost + tax;
+            Console.WriteLine("===============================");
+            Console.WriteLine($"Total estimate: ${total}");
+            Console.WriteLine("This estimate is valid for 30 days");
+        }
+    }
+}
